9db453ae2aea1d5b9e9e071c40f92589a58752ed
Denis <s90190003@gse.cs.msu.ru>
2022-03-13 14:04:49 +0300

    Commit 2, homework 14.02.2022
    +Сделан вывод последнего коммита

--- 20220214/1/prog.py
+++ 20220214/1/prog.py
@@ -3,6 +3,17 @@ import sys
 from glob import iglob
 from os.path import basename, dirname
 
-for dr in iglob('.git/refs/heads/*'):
-	print(basename(dr))
+if len(sys.argv) == 1:
+	for dr in iglob('.git/refs/heads/*'):
+		print(basename(dr))
+else:
+	branch = sys.argv[1]
+	commit_name = ''
+	with open(f'.git/refs/heads/{branch}/') as bp:
+		commit_name = bp.readline().strip()
+	commit_dir, commit_file = commit_name[:2], commit_name[2:]
+	obj = ''
+	with open(f'.git/objects/{commit_dir}/{commit_name_file}/', 'rb') as cf:
+		obj = zlib.decompress(cf.read())
+	print(obj.decode())
 
