7d225cc5839298dc3d3b6067fbecb77b6925ee0a
Denis <s90190003@gse.cs.msu.ru>
2022-03-13 14:15:58 +0300

    Commit 3, homework 14.02.2022
    +Сделан вывод дерева, на который указывает последний коммит выбранной ветки

--- 20220214/1/prog.py
+++ 20220214/1/prog.py
@@ -3,6 +3,8 @@ import sys
 from glob import iglob
 from os.path import basename, dirname
 
+SHIFT = ' '
+
 if len(sys.argv) == 1:
 	for dr in iglob('.git/refs/heads/*'):
 		print(basename(dr))
@@ -15,5 +17,24 @@ else:
 	obj = ''
 	with open(f'.git/objects/{commit_dir}/{commit_name_file}/', 'rb') as cf:
 		obj = zlib.decompress(cf.read())
-	print(obj.decode())
+	objtext = obj.decode()
+	print(objtext)
+	_, objtxt = objtext.split('tree ')
+
+	if 'parent' in objtext:
+		nametree, _ = objtxt.split('\nparent')
+	else:
+		nametree, _ = objtxt.split('\nauthor')
+	print('tree', nametree)
+	
+	nametree_dir, nametree_file = nametree[:2], nametree[2:]
+	obj = ''
+	with open(f'.git/objects/{nametree_dir}/{nametree_file}/', 'rb') as nf:
+		obj = zlib.decompress(nf.read())
+	_, _, tail = obj.partition(b'\x00')
+	while tail:
+		treeobj, _, tail = tail.partition(b'\x00')
+		tmode, tname = treeobj.split()
+		num, tail = tail[:20], tail[20:]
+		print(f"{SHIFT}{tname.decode()} {tmode.decode()} {num.hex()}")
 
